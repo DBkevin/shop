0e77c830f9cb6c27a44ef9786b380ebfa40a909b
Lee <416606903@outlook.com>
2019-07-10 03:49:34 +0000

    用户界面-订单列表页

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,5 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('cart', 'CartController@index')->name('cart.index');
     Route::delete('cart/{sku}','CartController@remove')->name('cart.remove');
     Route::post('orders','OrdersController@store')->name('order.store');
+    Route::get('orders','OrdersController@index')->name('orders.index');
 });
