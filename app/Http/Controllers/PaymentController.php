6cfb5be16fa9ff07e620a46221b5850b2e99d7c5
Lee <416606903@outlook.com>
2019-07-11 10:12:21 +0000

    微信支付

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,63 +6,113 @@ use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Exceptions\InvalidRequestException;
 use Carbon\Carbon;
+use Endroid\QrCode\QrCode;
 
 class PaymentController extends Controller
 {
     //
-    public function payByAlipay(Order $order,Request $request){
+    public function payByAlipay(Order $order, Request $request)
+    {
         //判断订单是否输于当前用户
-        $this->authorize('own',$order);
+        $this->authorize('own', $order);
         //订单已支付或者关闭
-        if($order->paid_at ||$order->closed){
+        if ($order->paid_at || $order->closed) {
             throw new InvalidRequestException('订单状态不正确');
         }
         //调用支付宝的网页支付
         return app('alipay')->web([
-            'out_trade_no'=>$order->no,//订单编号,需保证在商户端不重复
-            'total_amount'=>$order->total_amount,//订单金额,单位元,支持小数点后2位
-            'subject'=>'支付Laravel Shop的订单'.$order->items[0]->product->title,//订单标题
+            'out_trade_no' => $order->no, //订单编号,需保证在商户端不重复
+            'total_amount' => $order->total_amount, //订单金额,单位元,支持小数点后2位
+            'subject' => '支付Laravel Shop的订单' . $order->items[0]->product->title, //订单标题
         ]);
     }
     //前端回调
-    public function alipayReturn(){
-       try {
-           //
-           app('alipay')->verify();
-       } catch (\Exception $e) {
-           //throw $th;
-           return view('pages.error',['msg'=>'数据不正确']);
-       }
+    public function alipayReturn()
+    {
+        try {
+            //
+            app('alipay')->verify();
+        } catch (\Exception $e) {
+            //throw $th;
+            return view('pages.error', ['msg' => '数据不正确']);
+        }
 
-       return view('pages.success',['msg'=>'付款成功']);
+        return view('pages.success', ['msg' => '付款成功']);
     }
 
     //服务端回调
-    public function alipayNotify(){
+    public function alipayNotify()
+    {
         //校验输入参数
-        $data=app('alipay')->verify();
+        $data = app('alipay')->verify();
         //如果订单状态不是成功或者结束,则不走后续的逻辑
-        if(!in_array($data->trade_status,['TRADE_SUCCESS','TRADE_FINISHED'])){
+        if (!in_array($data->trade_status, ['TRADE_SUCCESS', 'TRADE_FINISHED'])) {
             return  app('alipay')->success();
         }
         //$data->out_trade_no拿到流水号,并在数据库中查询
-        $order=Order::where('no',$data->out_trade_no)->first();
-           // 正常来说不太可能出现支付了一笔不存在的订单，这个判断只是加强系统健壮性。
-        if(!$order){
+        $order = Order::where('no', $data->out_trade_no)->first();
+        // 正常来说不太可能出现支付了一笔不存在的订单，这个判断只是加强系统健壮性。
+        if (!$order) {
             return 'fail';
         }
         //如果这笔订单的状态是已支付,
-        if($order->paid_at){
+        if ($order->paid_at) {
             //返回数据给支付宝
             return  app('alipay')->success();
         }
         $order->update([
-            'paid_at'=>Carbon::now(),//支付时间
-            'payment_method'=>'alipay',//支付方式
-            'payment_no'=>$data->trade_no,//支付宝订单号
+            'paid_at' => Carbon::now(), //支付时间
+            'payment_method' => 'alipay', //支付方式
+            'payment_no' => $data->trade_no, //支付宝订单号
         ]);
 
         return app('alipay')->success();
+    }
 
+
+    //微信支付
+    public function payByWechat(Order $order, Request $request)
+    {
+        //权限校验
+        $this->authorize('own', $order);
+        //校验订单状态
+        if ($order->paid_at || $order->closed) {
+            throw new  InvalidRequestException('订单状态不正确');
+        }
+        //scan 方法为拉起微信扫码付款 
+       $wechatOrder=app('wechat_pay')->scan([
+            'out_trade_no' => $order->no, //商户订单流水号,与支付宝订单our_trade_no一致
+            'total_fee' => $order->total_amount * 100, //单位分
+            'body' => '支付shop的订单:' . $order->items[0]->product->title,
+        ]);
+        $qrCode=new QrCode($wechatOrder->code_url);
+        //将生成的二维码图片以数据字符串形式输出,并带上响应的响应类型
+        return response($qrCode->writeString(),200,['Content-Type'=>$qrCode->getContentType()]);
+    }
+
+    public function wechatNotify()
+    {
+        //校验回调参数是否正确
+        $data = app('wechat_pay')->verify();
+        $order = Order::where('no', $data->out_trade_no)->first();
+
+        //订单不存在告知微信支付
+        if (!$order) {
+            return 'fail';
+        }
+        //订单支付
+        if ($order->paid_at) {
+            //告知微信支付此订单已经支付过了
+            return app('wechat_pay')->success();
+        }
+
+        //将订单标记为支付
+        $order->update([
+            'paid_at' => Carbon::now(),
+            'payment_method' => 'wechat',
+            'payment_no' => $data->transaction_id,
+        ]);
+
+        return app('wechat_pay')->success();
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,16 +11,16 @@
     <!-- 样式 -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
-    <div id="app" class="{{ route_class() }}-page">
+   <div id="app" class="{{ route_class() }}-page">
         @include('layouts._header')
         <div class="container">
             @yield('content')
         </div>
         @include('layouts._footer')
     </div>
-    <!-- JS 脚本 -->
+</body>
     <script src="{{ mix('js/app.js') }}"></script>
     @yield('scriptAfterJs')
-</body>
 </html>
