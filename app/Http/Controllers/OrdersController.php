c1ce84d55fd1fe310f16742d73cc1765ec411983
Lee <416606903@outlook.com>
2019-07-15 06:54:44 +0000

    用户界面-申请退款

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -11,6 +11,7 @@ use App\Exceptions\InvalidRequestException;
 use Carbon\Carbon;
 use App\Http\Requests\SendReviewRequest;
 use App\Events\OrderReviewed;
+use App\Http\Requests\ApplyRefundRequest;
 
 class OrdersController extends Controller
 {
@@ -101,65 +102,93 @@ class OrdersController extends Controller
         return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
     }
 
-    public function received(Order $order,Request $request){
+    public function received(Order $order, Request $request)
+    {
         //校验权限
-        $this->authorize('own',$order);
+        $this->authorize('own', $order);
 
         //判断订单的发货状态是否是已支付
-        if($order->ship_status !==Order::SHIP_STATUS_DELIVERED){
+        if ($order->ship_status !== Order::SHIP_STATUS_DELIVERED) {
             throw new InvalidRequestException('发货状态不正确');
         }
 
         //更新发货状态为已收到
-        $order->update(['ship_status'=>Order::SHIP_STATUS_RECEIVED]);
+        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
 
         //返回原页面
         return $order;
     }
 
-    public function review(Order $order){
+    public function review(Order $order)
+    {
         //检验权限
-        $this->authorize('own',$order);
+        $this->authorize('own', $order);
         //判断是否已经支付
-        if(!$order->paid_at){
+        if (!$order->paid_at) {
             throw new InvalidRequestException('该订单还未支付,不能评价');
         }
 
         //使用laod方法加载关联数据,避免N+1的 问题
-        return view('orders.review',['order'=>$order->load(['items.productSku','items.product'])]);
+        return view('orders.review', ['order' => $order->load(['items.productSku', 'items.product'])]);
     }
-    public function sendReview(Order $order,SendReviewRequest $request){
+    public function sendReview(Order $order, SendReviewRequest $request)
+    {
         //校验权限,
-        $this->authorize('own',$order);
-        if(!$order->paid_at){
+        $this->authorize('own', $order);
+        if (!$order->paid_at) {
             throw new InvalidRequestException('该订单还未支付,不可评价');
         }
         //判断是否评价过了
-        if($order->reviewed){
+        if ($order->reviewed) {
             throw new InvalidRequestException('该订单已评价,不可重复提交');
         }
 
         $reviews = $request->input('reviews');
 
         //开启事务,
-        \DB::transaction(function () use ($reviews,$order) {
+        \DB::transaction(function () use ($reviews, $order) {
             //遍历用户提交的数据
             foreach ($reviews as $review) {
                 # code...
-                $orderItem=$order->items()->find($review['id']);
+                $orderItem = $order->items()->find($review['id']);
                 //保存评分和评价
                 $orderItem->update([
-                    'rating'=>$review['rating'],
-                    'review'=>$review['review'],
-                    'reviewed_at'=>Carbon::now(),
+                    'rating' => $review['rating'],
+                    'review' => $review['review'],
+                    'reviewed_at' => Carbon::now(),
                 ]);
             }
 
             //将订单标记未已评价
-            $order->update(['reviewed'=>true]);
+            $order->update(['reviewed' => true]);
             event(new OrderReviewed($order));
         });
 
         return redirect()->back();
+    }
+
+    public function applyRefund(Order $order, ApplyRefundRequest $request)
+    {
+        //校验订单是否属于用户
+        $this->authorize('own', $order);
+
+        //判断订单是否已经支付
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付,不可退款');
+        }
+        //判断订单状态是否正确
+        if ($order->refund_status !== Order::REFUND_STATUS_PENDING) {
+            throw new InvalidRequestException("该订单已经申请退款了,不能重复申请,");
+        }
+        //将用户输入的退款理由放到订单的extra字段中
+        $extra = $order->extra ?: [];
+        $extra['refund_reason'] = $request->input('reason');
+        //将订单状态需改为已退款
+        $order->update([
+            'refund_status' => Order::REFUND_STATUS_APPLIED,
+            'extra' => $extra,
+        ]);
+
+        return $order;
     }
 }
